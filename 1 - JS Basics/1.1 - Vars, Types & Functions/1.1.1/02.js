19ef25b972ac8799917c31dc84cf4ffaefb07941
suriiikim <116673836+suriiikim@users.noreply.github.com>
2023-02-14 21:51:14 +0100

    Update 02.js

--- 1 - JS Basics/1.1 - Vars, Types & Functions/1.1.1/02.js
+++ 1 - JS Basics/1.1 - Vars, Types & Functions/1.1.1/02.js
@@ -4,3 +4,7 @@ let a = 4;
 a = 5;
 
 console.log(a);
+
+
+// 'const' cannot be changed; NEVER. Therefore 'const' cannot declare 'a' to value of '4' and subsequently declare 'a' to '5'.
+// 'const' variable can be replaced with a regular variable to 'let'.
