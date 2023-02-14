ebe7107ccc056536230fabc374ca4f09a7ca7d4c
suriiikim <116673836+suriiikim@users.noreply.github.com>
2023-02-14 23:18:11 +0100

    Update 01.js

--- 1 - JS Basics/1.1 - Vars, Types & Functions/1.1.7/01.js
+++ 1 - JS Basics/1.1 - Vars, Types & Functions/1.1.7/01.js
@@ -1,10 +1,16 @@
-// Crea una función que devuelva el texto proporcionado en mayúsculas
+// Crea una función que devuelva el texto proporcionado en mayúsculas 
+
+// 'toUppercase' convierte una string en mayúsculas.
 
 // 👇🏻 Define aquí tu función
-const toUppercase = () => {
-
+const toUppercase = (text) => { // La function recibe un parametro 'text' que es la string que se va a convertir en mayus. 
+                               // Se llama al metodo 'toUppercase()' en parametro 'text' return 
+  return text.toUppercase();  // return el resultado.
 };
 // 👆🏻
 
-toUppercase('hola que tal'); // HOLA QUE TAL
+toUppercase('hola que tal'); // HOLA QUE TAL 
 toUppercase('me llamo Carlos'); // ME LLAMO CARLOS
+
+console.log (toUppercase('hola que tal'));
+console.log (toUppercase('me llamo Carlos'));
