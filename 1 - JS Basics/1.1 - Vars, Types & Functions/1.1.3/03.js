18ee2a408e30fa5d57e3f0a95055852194e0e4a4
suriiikim <116673836+suriiikim@users.noreply.github.com>
2023-02-14 22:12:47 +0100

    Update 03.js
    '-Math.abs()'

--- 1 - JS Basics/1.1 - Vars, Types & Functions/1.1.3/03.js
+++ 1 - JS Basics/1.1 - Vars, Types & Functions/1.1.3/03.js
@@ -2,3 +2,10 @@
 
 inverse(4) // Debe mostrar -4
 inverse(2) // Debe mostrar -2
+
+function inverse(number) { // el parametro de esta function is 'number'. 
+  return -Math.abs(number); // la expresión '-Math.abs()' dará resultado de numero con un '-'. 
+}
+
+console.log(inverse(4));
+console.log(inverse(2));
