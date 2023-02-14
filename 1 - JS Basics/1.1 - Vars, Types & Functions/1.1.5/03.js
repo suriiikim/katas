c171e0a6b0fe88805000765148fc2339ba3162ff
suriiikim <116673836+suriiikim@users.noreply.github.com>
2023-02-14 22:35:14 +0100

    Update 03.js

--- 1 - JS Basics/1.1 - Vars, Types & Functions/1.1.5/03.js
+++ 1 - JS Basics/1.1 - Vars, Types & Functions/1.1.5/03.js
@@ -1,11 +1,11 @@
 // Que crees que mostrará counter?
 // Podrías cambiar el código de alguna manera para que muestre 5?
+
 const createCounter = () => {
-  const counter = 5;
-  return counter;
+  const counter = 5; //variable 'const counter' solo existe dentro de la function 'createCounter'. Por lo tanto, va a dar un error.
+  return counter; // 'counter' no está definido en ámbito global.
 }
 
-createCounter();
+const counter= createCounter(); // Para que muestre 5, hay que crear una línea para almacenar el valor de return por la function 'createCounter' en una variable.
 
-// ❓
-console.log(counter);
+console.log(counter); // Aquí la function 'createCounter' return el varlor '5', que se almacena en la variable 'counter', llamando la function.
