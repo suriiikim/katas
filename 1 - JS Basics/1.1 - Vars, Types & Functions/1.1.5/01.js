bcb659e479be99b2e6b50ba60de68003655b9bc0
suriiikim <116673836+suriiikim@users.noreply.github.com>
2023-02-14 22:27:29 +0100

    Update 01.js

--- 1 - JS Basics/1.1 - Vars, Types & Functions/1.1.5/01.js
+++ 1 - JS Basics/1.1 - Vars, Types & Functions/1.1.5/01.js
@@ -1,15 +1,15 @@
 // Revisa este código y añade al final el nombre que crees que contendrá
 // la variable "finalName" y la variable "name"
-const name = 'Pepe';
+const name = 'Pepe'; // Se define la variable 'name' como Pepe
 
-const primera = () => {
-  const name = 'Jon';
-  return name;
+const primera = () => { // Se define la function llamada 'primera' que establece la variable 'name' como 'Jon'.
+  const name = 'Jon'; 
+  return name; // Devuelve el valor 'name'
 };
 
-const finalName = primera();
+const finalName = primera(); // La function se llama y su valor de return se guarda en la variable 'finalName'.
 
 // 👇🏻 Añade entre las comillas el nombre que contiene finalName y name;
-console.log(finalName, '');
-console.log(name, '');
+console.log(finalName, 'Jon');
+console.log(name, 'Pepe');
 // 👆🏻
