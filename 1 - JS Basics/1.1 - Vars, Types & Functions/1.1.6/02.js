5f36a4798dfeb9521733814123ba62c59101f844
suriiikim <116673836+suriiikim@users.noreply.github.com>
2023-02-14 22:59:49 +0100

    Update 02.js

--- 1 - JS Basics/1.1 - Vars, Types & Functions/1.1.6/02.js
+++ 1 - JS Basics/1.1 - Vars, Types & Functions/1.1.6/02.js
@@ -1,11 +1,16 @@
-const user = {
-  name: 'Juan',
-  age: 36,
+const user = { //'user' es el objeto
+  name: 'Juan', // propiedad del objeto 1
+  age: 36, // propiedad del objeto 2
 };
 
-// Muestra por consola el nombre del user
-
-// Muestra por consola la edad del user
-
 // Añade una propiedad "city" al objeto user con el valor que quieras
+user.city = 'Valencia';
+
+// Muestra por consola el nombre del user
+console.log (user.name); // Se usa la notación de puntos
+// Muestra por consola la edad del user
+console.log (user.age);
+
 // y muestra el objeto por consola
+
+console.log (user);
