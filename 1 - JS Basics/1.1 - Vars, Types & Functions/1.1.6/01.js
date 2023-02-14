ca897c428ec19cf15e50641312df9eb4c473aa0d
suriiikim <116673836+suriiikim@users.noreply.github.com>
2023-02-14 22:54:03 +0100

    Update 01.js

--- 1 - JS Basics/1.1 - Vars, Types & Functions/1.1.6/01.js
+++ 1 - JS Basics/1.1 - Vars, Types & Functions/1.1.6/01.js
@@ -2,4 +2,4 @@
 const listaCompra = ['🍎', '🌽', '🥬'];
 
 // Muestra aqui la lechuga accediendo al array "listaCompra"
-console.log(listaCompra[1]);
+console.log(listaCompra[2]); //sería '2' en lugar de '1' porque un array empieza en '0'. 
