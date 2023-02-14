c487c3ccd2cdb363a26a459b4d2b1a515205ec6f
suriiikim <116673836+suriiikim@users.noreply.github.com>
2023-02-14 22:51:02 +0100

    Update 02.js

--- 1 - JS Basics/1.1 - Vars, Types & Functions/1.1.5/02.js
+++ 1 - JS Basics/1.1 - Vars, Types & Functions/1.1.5/02.js
@@ -1,17 +1,17 @@
 // Modifica la variable counter al invocar la función para que el numero mostrado sea un 1
 
-const counter = 0;
+// Primero, hay que establecer el valor de 'counter' en '1'.
 
-const sumCounter = () => {
+let counter = 0; // La variable 'counter' inicializa en '0'.
+
+const sumCounter = () => { // Luego declara la function 'sumCounter' que establece el valor de 'counter' en '1'.
   // 👇🏻 Modifica aquí la variable counter
-
-  // 👆🏻
+    counter= 1;
 };
-
-console.log(counter);
+  // 👆🏻
 
 // 👇🏻 Invoca la función para modificarlo
-
+sumCounter();
 // 👆🏻
 
 console.log(counter);
