9023c56ff5c9dfef42e021444fbc27d9e52c8f9b
suriiikim <116673836+suriiikim@users.noreply.github.com>
2023-02-14 23:33:56 +0100

    Update 03.js

--- 1 - JS Basics/1.1 - Vars, Types & Functions/1.1.7/03.js
+++ 1 - JS Basics/1.1 - Vars, Types & Functions/1.1.7/03.js
@@ -1,11 +1,21 @@
 // Muestra la longitud de los siguientes textos por consola
 
+// '.length' define la longitud
+
 const text1 = "Hola"; // 4
 const text2 = 'Que tal'; // 7
 const text3 = `   EY    ` // 9
 
 // 👇🏻 Añade aqui los logs de la longitud de los textos
 
+console.log(text1.length); // '.length' define la longitud
+console.log(text2.length);
+console.log(text3.length);
+
 // 👆🏻
 
 // 👀 Te animas a mostrar true si la longitud del texto es mayor a 5 o false en el caso contrario?
+
+console.log(text1.length > 5); // false
+console.log(text2.length > 5); // true
+console.log(text3.length > 5); // true
