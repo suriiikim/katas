50eaf638413117efd236295b8cb26e688db1e411
suriiikim <116673836+suriiikim@users.noreply.github.com>
2023-02-14 22:05:18 +0100

    Update 01.js

--- 1 - JS Basics/1.1 - Vars, Types & Functions/1.1.3/01.js
+++ 1 - JS Basics/1.1 - Vars, Types & Functions/1.1.3/01.js
@@ -6,6 +6,14 @@ const sayHi = function() {
 
 // 👇🏻 Invoca la función aquí
 
-// 👆🏻
+// Al invocar esta función, se ejecutará el código que se encuentra dentro de ella, 
+//que en este caso es una única instrucción que escribe 'Hi!' en la consola.
+
+sayHi ();
+// 👆🏻 // La función sayHi no tiene parámetros, por lo que no es necesario pasarle ningún valor entre los paréntesis.
 
 // ✅  => Hi!
+
+// La razón por la que es importante invocar la función es que, aunque el código para definirla existe, 
+//no se ejecutará hasta que la función sea invocada en algún momento del programa.
+// Si la función no se invoca, el código dentro de ella nunca se ejecutará y no se mostrará el mensaje 'Hi!' en la consola.
