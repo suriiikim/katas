9de704d7735e247298fa29a6366186c8827e54c7
suriiikim <116673836+suriiikim@users.noreply.github.com>
2023-02-14 23:07:42 +0100

    Update 03.js

--- 1 - JS Basics/1.1 - Vars, Types & Functions/1.1.6/03.js
+++ 1 - JS Basics/1.1 - Vars, Types & Functions/1.1.6/03.js
@@ -15,6 +15,6 @@ const users = [
 ];
 
 // Escribe entre comillas, que crees que aparecerá en los siguiente log y descomentalo
-// console.log(users[0].age, '');
-// console.log(users.name, '');
-// console.log(users[2].name, '');
+// console.log(users[0].age, ''); // 36. Muestra edad de user '0', 'Juan'.
+// console.log(users.name, ''); // undefined. Ya que 'users' es un array y NO tiene propiedad 'name'
+// console.log(users[2].name, ''); // Paco. Muestra el nombre de user '2'.
